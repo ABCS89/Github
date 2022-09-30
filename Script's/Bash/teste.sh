51e4e8c291c1efbf55328cb08fcb2ddcb3e62297
victornico15 <vnespindola15@gmail.com>
2022-09-30 15:28:18 -0300

    Bash upscale

---
+++ Script's/Bash/teste.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+run_bootstrap="True"
+
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
+
+
+
+# verificação caso não deseja que rode o bootstrap-playbook.yml
+playbook_name=""
+if [ ${run_bootstrap^^} == "FALSE" ];
+then
+    playbook_name="main.yml" 
+else
+    playbook_name="bootstrap-playbook.yml"  
+fi
+
+
+
