8176b1d55ac118e58774caa07e68ceb97ed31efd
victor.nico15 <vnespindola15@gmail.com>
2022-10-28 08:54:27 -0300

    save

--- Script/Bash/Scripts_feitos/yaml_chamandoPythonFunc.sh
+++ Script/Bash/Scripts_feitos/yaml_chamandoPythonFunc.sh
@@ -1,11 +1,9 @@
 yaml() {
     python3 -c "import yaml;print(yaml.safe_load(open('$1'))$2)"
 }
-
-sudo ansible-vault decrypt /etc/korp/ansible/inventory.yml --vault-id /etc/korp/ansible/.vault_key
-VALUE=$()
+VALUE=$(yaml)
 echo $VALUE
-sudo ansible-vault encrypt /etc/korp/ansible/inventory.yml --vault-id /etc/korp/ansible/.vault_key
+
 
 
 
