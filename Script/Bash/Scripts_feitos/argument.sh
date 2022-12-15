84c55bb400c081b3dc9c5a737d38fc2b7956fb4c
victor.nico15 <vnespindola15@gmail.com>
2022-12-15 15:13:00 -0300

    OK

--- Script/Bash/Scripts_feitos/argument.sh
+++ Script/Bash/Scripts_feitos/argument.sh
@@ -15,11 +15,15 @@ done
 
 # verificação caso não deseja que rode o bootstrap-playbook.yml
 playbook_name=""
-if [ ${run_bootstrap^^} == "FALSE" ];
+if [ ${run_bootstrap^^} == "false" ]; 
+#    [ ${run_bootstrap,,} == "false" ]; LOWERCAISE COM ,,
+#if [ ${run_bootstrap^^} == "false" ]; UPERCASE COM ^^
 then
-    playbook_name="main.yml" 
+    #playbook_name="main.yml" 
+    echo "1 "
 else
-    playbook_name="bootstrap-playbook.yml"  
+    #playbook_name="bootstrap-playbook.yml"
+     echo "2 "  
 fi
 
 
