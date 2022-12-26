03785f00a9d97a179ccaf7e2715ba589ff2a026d
victornico15 <vnespindola15@gmail.com>
2022-12-26 15:52:29 -0300

    OK

---
+++ terraform/aws/Arquitetura/Provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  version = "~> 4.0"
+  region  = "us-east-1"
+  profile = "terraform-aws"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
+
+#DOCUMENTACAO https://registry.terraform.io/providers/hashicorp/aws/latest/docs
