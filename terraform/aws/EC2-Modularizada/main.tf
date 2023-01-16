683582ee96c1bf3ad89039f6224253b2a2302b74
victornico15 <vnespindola15@gmail.com>
2023-01-16 15:14:48 -0300

    ok

--- terraform/aws/EC2-Modularizada/main.tf
+++ terraform/aws/EC2-Modularizada/main.tf
@@ -1,22 +1,9 @@
 provider "aws" {
   region = "us-east-1"
 }
-variable "key_name" {default="my-key"}
-resource "tls_private_key" "example" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-resource "aws_key_pair" "generated_key" {
-  key_name   = "${var.key_name}"
-  public_key = "${tls_private_key.example.public_key_openssh}"
-}
-resource "local_file" "private_key" {
-  content  = "${tls_private_key.example.public_key_openssh}"
-  filename = "../ssh/private_key.pem"
-}
-
 
 module Taxman {
-  source = "./aplicacao/taxman"  
+  source = "./aplicacao/taxman"   
+  
 }
 
