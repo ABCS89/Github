54c57e98200f4e4927837ec09f7008c958df25b4
victornico15 <vnespindola15@gmail.com>
2023-01-17 11:49:10 -0300

    ok

--- terraform/aws/CodeArtifact/main.tf
+++ terraform/aws/CodeArtifact/main.tf
@@ -1,13 +1,13 @@
+#KMSKEY para o Repo_NPM
 resource "aws_kms_key" "code_artifact_encryption_key" {
   description = "Chave encriptada para acesso ao dominio Repo_NPM"
 }
-
 #Dominio criado para Repo_NPM
 resource "aws_codeartifact_domain" "my_domain" { 
   domain = "my-domain"
   encryption_key = aws_kms_key.code_artifact_encryption_key.arn
-
 }
+#Reposito NPM
 resource "aws_codeartifact_repository" "Repo_NPM" {
   description = "Repo_NPM"
   domain = aws_codeartifact_domain.my_domain.domain
