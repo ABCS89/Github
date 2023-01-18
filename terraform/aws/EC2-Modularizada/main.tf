8f385d125414fac0543e63149b7ebc2e33ed918f
victornico15 <vnespindola15@gmail.com>
2023-01-17 21:23:37 -0300

    ok

--- terraform/aws/EC2-Modularizada/main.tf
+++ terraform/aws/EC2-Modularizada/main.tf
@@ -2,8 +2,45 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module Taxman {
-  source = "./aplicacao/taxman"   
-  
+module "Taxman" {
+  source                   = "./aplicacao/taxman"
+  Taxman_EC2_name          = var.Taxman_EC2_name
+  Taxman_EC2_ami           = var.Taxman_EC2_ami
+  Taxman_EC2_instance_type = var.Taxman_EC2_instance_type
+  provider_region          = var.provider_region
+  availability_zone        = var.availability_zone
 }
+
+
+
+module "contractus" {
+  source                   = "./aplicacao/taxman"
+  Taxman_EC2_name          = var.Taxman_EC2_name
+  Taxman_EC2_ami           = var.Taxman_EC2_ami
+  Taxman_EC2_instance_type = var.Taxman_EC2_instance_type
+  provider_region          = var.provider_region
+  availability_zone        = var.availability_zone
+}
+
+
+
+module "bookeper" {
+  source                   = "./aplicacao/taxman"
+  Taxman_EC2_name          = var.Taxman_EC2_name
+  Taxman_EC2_ami           = var.Taxman_EC2_ami
+  Taxman_EC2_instance_type = var.Taxman_EC2_instance_type
+  provider_region          = var.provider_region
+  availability_zone        = var.availability_zone
+}
+
+
+module "loadbalancer" {
+  source                   = "./aplicacao/taxman"
+  Taxman_EC2_name          = var.Taxman_EC2_name
+  Taxman_EC2_ami           = var.Taxman_EC2_ami
+  Taxman_EC2_instance_type = var.Taxman_EC2_instance_type
+  provider_region          = var.provider_region
+  availability_zone        = var.availability_zone
+}
+
 
